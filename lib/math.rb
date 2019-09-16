0b78b8df2f1bdce0bd98536d892b9b9658918e9c
Tyler Carroll <tylercarroll1611@gmail.com>
2019-09-16 10:59:19 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
+addition(9, 10)
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
+subtraction(13, 10)
 
 def division(num1, num2)
-
+  num1 / num2 
 end
+division(15, 3)
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
+multiplication(3, 5)
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
+modulo(12, 3)
 
 def square_root(num)
-
+  num
 end
+square_root(81)
